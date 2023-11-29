dc7dc7c061ff7cd56ecf8005c5273fc50a67d21f
leejm9704 <leejm9704@naver.com>
2023-11-29 15:08:02 +0900

    [env] profile 프로젝트 기초 환경 설정

--- develop/profile/src/App.tsx
+++ develop/profile/src/App.tsx
@@ -8,16 +8,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Profile 메인페이지 입니다. 마음껏 꾸며보세요
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
